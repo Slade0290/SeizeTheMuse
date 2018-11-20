cb65acd85c6ab25c4c0af305b0e94f9697e29c40
Slade <ly.david@live.fr>
2018-11-20 15:50:34 +0100

    creation des classes

---
+++ SeizeTheMuse/SeizeTheMuse/Note.swift
@@ -0,0 +1,27 @@
+//
+//  Note.swift
+//  SeizeTheMuse
+//
+//  Created by LY David on 20/11/2018.
+//  Copyright © 2018 LY David. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    
+    let name : String
+    let frequence : Double
+    let altered : Bool
+    
+    init(name : String, frequence : Double, altered : Bool) {
+        self.name = name
+        self.frequence = frequence
+        self.altered = altered
+        //let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+        //let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+        //let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
+        //Voir comment faire pour controler/limiter la création de note
+    }
+    
+}
