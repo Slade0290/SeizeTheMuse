e3aa1caf44b37bcb61beb68d1a5f239b001ce72b
Slade <ly.david@live.fr>
2018-11-26 11:10:44 +0100

    add ViewController.swift

---
+++ SeizeTheMuse/ViewController.swift
@@ -0,0 +1,24 @@
+//
+// ViewController.swift
+// SeizeTheMuse
+//
+//
+// Created by Bastien EL GANA on 21/11/2018
+// Copyright 2018 LY David. All rights reserved
+
+import UIKit
+import AudioKit
+
+class ViewController: UIViewController {
+
+	override func viewWillDisappear(_ animated : Bool) {
+		super.viewWillDisappear(animated)
+		UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
+	}
+
+ 	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+}
+
