640680ad1f0374ab9406bb173840aa3ade160d3b
Clement <pota@Clements-Mac.local>
2018-11-26 01:16:36 -0800

    add db

---
+++ Pods/Target Support Files/Pods-SeizeTheMuse/Pods-SeizeTheMuse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeizeTheMuse : NSObject
+@end
+@implementation PodsDummy_Pods_SeizeTheMuse
+@end
---
+++ Pods/Target Support Files/Pods-SeizeTheMuseTests/Pods-SeizeTheMuseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeizeTheMuseTests : NSObject
+@end
+@implementation PodsDummy_Pods_SeizeTheMuseTests
+@end
