b9b88bf194a2f0799a95772feda3b6e4dadb20fa
bastien458 <bastien.elgana@ynov.com>
2018-11-26 16:54:48 +0100

    interface responsive

--- SeizeTheMuse/ViewController.swift
+++ SeizeTheMuse/ViewController.swift
@@ -7,18 +7,19 @@
 // Copyright 2018 LY David. All rights reserved
 
 import UIKit
-import AudioKit
+//import AudioKit
 
 class ViewController: UIViewController {
 
-	override func viewWillDisappear(_ animated : Bool) {
-		super.viewWillDisappear(animated)
-		UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
-	}
+
 
  	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
+    
+//    override var shouldAutorotate: Bool {
+//        return false
+//    }
 
 }
 
