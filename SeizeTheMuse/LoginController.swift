7327ab101e1d1b682e8327a36b0b3b3042cf1d8e
Clement <pota@Clements-Mac.local>
2018-12-04 01:54:42 -0800

    DataBase

--- SeizeTheMuse/LoginController.swift
+++ SeizeTheMuse/LoginController.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 //import AudioKit
+import FirebaseAuth
 
 class LoginController: UIViewController {
 
+
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle
     {
         return .lightContent
@@ -20,9 +25,29 @@ class LoginController: UIViewController {
 		super.viewDidLoad()
 	}
     
-//    override var shouldAuçtorotate: Bool {
-//        return false
-//    }
-
+    private func setupTextfieldsManaging() {
+        EmailTextField.delegate = self
+        PasswordTextField.delegate = self
+    }
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        Auth.auth().signIn(withEmail: EmailTextField.text!, password: PasswordTextField.text!) { (authResult, error) in
+            if error != nil {
+                print(error.debugDescription)
+            } else {
+                self.performSegue(withIdentifier: "GoToProfil2", sender: self)
+            }
+        }
+    }
+    
+    @IBAction func GoToSubscribe(_ sender: UIStoryboardSegue) {
+        performSegue(withIdentifier: "GoToSub", sender: nil)
+    }
 }
 
+extension LoginController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
