90121caba21ed91ad2a68988ea177b3fae9b5259
Slade <ly.david@live.fr>
2018-12-04 12:08:22 +0100

    finishing class note

--- SeizeTheMuse/Note.swift
+++ SeizeTheMuse/Note.swift
@@ -28,67 +28,65 @@ struct Note {
         switch name {
         // Do dans la 6e ligne
             case "C" :
-                self.frequence = mathFrequency(32.70, octave)
-                self.position = 0
+                self.frequence = self.mathFrequency(32.70, octave)
+                self.position = mathPosition(93, octave)
             
         // Do # dans la 6e ligne
             case "C#" :
                 self.frequence = self.mathFrequency(34.65, octave)
-                self.position = 0
+                self.position = mathPosition(93, octave)
             
         // Ré sur la 6e ligne
             case "D" :
                 self.frequence = self.mathFrequency(36.71, octave)
-                self.position = 0
+                self.position = mathPosition(88, octave)
             
         // Ré # sur la 6e ligne
             case "D#" :
                 self.frequence = self.mathFrequency(38.89, octave)
-                self.position = 0
+                self.position = mathPosition(88, octave)
             
         // Mi dans la 5e ligne
             case "E" :
                 self.frequence = self.mathFrequency(41.20, octave)
-                self.position = 0
+                self.position = mathPosition(83, octave)
             
         // Fa sur la 5e ligne
             case "F" :
                 self.frequence = self.mathFrequency(43.65, octave)
-                self.position = 0
+                self.position = mathPosition(78, octave)
             
         // Fa # sur la 5e ligne
             case "F#" :
                 self.frequence = self.mathFrequency(46.25, octave)
-                self.position = 0
+                self.position = mathPosition(78, octave)
         
         // Sol dans la 4e ligne
             case "G" :
                 self.frequence = self.mathFrequency(49.00, octave)
-                self.position = 0
+                self.position = mathPosition(73, octave)
         // Sol # dans la 4e ligne
             case "G#" :
                 self.frequence = self.mathFrequency(51.91, octave)
-                self.position = 0
+                self.position = mathPosition(73, octave)
         
         // La sur la 4e ligne
             case "A" :
                 self.frequence = self.mathFrequency(55.00, octave)
-                self.position = 0
+                self.position = mathPosition(68, octave)
         
         // La # sur la 4e ligne
             case "A#" :
                 self.frequence = self.mathFrequency(58.27, octave)
-                self.position = 0
+                self.position = mathPosition(68, octave)
         
         // Si dans la 3e ligne
             case "B" :
                 self.frequence = self.mathFrequency(61.74, octave)
-                self.position = 0
+                self.position = mathPosition(63, octave)
             default:
                 break
         }
-        
-        // if position > depasse de la portée alors il faut dessiner des traits en plus en foncion de l'octave
     }
     
     func mathFrequency(_ frequency: Double, _ octave: Int ) -> Double {
@@ -99,9 +97,12 @@ struct Note {
         return value
     }
     
-    func mathPosition(_ name: String, _ octave: Int) -> Int{
-        var value = 0
-            value = 1 // for ? because octave 3 position define then if octave sup then val="get pts between 2 notes"
-        return value
+    //Ne gère que l'octave 3 & 4
+    func mathPosition(_ posY : Int, _ octave: Int) -> Int{
+        if octave == 3 {
+            return posY
+        } else {
+            return posY + 35
+        }
     }
 }
