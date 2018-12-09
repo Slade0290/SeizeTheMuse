7c8166fd95c2d53e5e40ad7de3bc3d82c91f42d9
Slade <ly.david@live.fr>
2018-12-09 16:59:00 +0100

    add result view controller and add function to switch view controller at the end of the song in partition view controller

---
+++ SeizeTheMuse/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  SeizeTheMuse
+//
+//  Created by LY David on 09/12/2018.
+//  Copyright © 2018 LY David. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var goodNotes: UILabel!
+    @IBOutlet weak var achievement: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
