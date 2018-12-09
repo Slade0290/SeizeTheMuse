9d4df0d2ca4b9a49d2a46c5984e5299460ea33a3
Slade <ly.david@live.fr>
2018-12-09 21:41:56 +0100

    create ProfilSongController

---
+++ SeizeTheMuse/ProfilSongController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfilSongController.swift
+//  SeizeTheMuse
+//
+//  Created by LY David on 09/12/2018.
+//  Copyright © 2018 LY David. All rights reserved.
+//
+
+import UIKit
+
+class ProfilSongController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
